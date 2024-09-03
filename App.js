4d6b083c115f20a4054fc78fea29ffb50524409b
devdeepak06 <k.deepak2217@gmail.com>
2024-09-03 07:49:08 +0530

    Ignited app using parcel and setup the configuration

---
+++ App.js
@@ -0,0 +1,22 @@
+/* 
+
+ReactElement(object) => HTML(Browser understands)
+*/
+import React from "react";
+import ReactDOM from "react-dom/client";
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "heading1" }, "This ia Namaste React🚀"),
+    React.createElement("h2", { id: "heading2" }, "I m an h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "heading3" }, "I m an h1 tag"),
+    React.createElement("h2", { id: "heading4" }, "I m an h2 tag"),
+  ])
+);
+// jsx
+// console.log(parent);+ //return object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Namaste React🚀
+
+
+# Parcel can do
+- Dev build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - which is written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minificstion 
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code splitting
+- Differential Bundling - support older browsers
+- Diagnostics - Beautiful errors
+- Error Handling
+- HTTPs 
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
